cbc420378a5a534273466d0860b654977825d289
ionutiulian2001 <ionutciobanu0106@gmail.com>
2023-10-15 21:44:18 +0300

    first commit

---
+++ EventApp/main2.js
@@ -0,0 +1,32 @@
+
+//alert("Welcome to the site!");
+
+
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
+
+button.onclick = function() {
+
+    if(content.className == "open")
+    { // shrink the box
+        content.className = "";
+        button.innerHTML = "Show more";
+        button.style.fontStyle = "italic";
+
+
+    } else 
+    { // expand the box
+        content.className = "open";
+        button.innerHTML = "Show less";
+        button.style.fontStyle = "italic";
+
+
+    }
+}
+
+
+/*button.onmouseover = function () {
+
+    alert("Dont hover here!! Just click!");
+}
+*/
